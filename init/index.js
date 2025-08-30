bb57c275d2496b2559480c03599dfcaa45b6ce67
Raghav Gupta <raghav01974@gmail.com>
2025-08-31 02:18:16 +0530

    Authorization Complete

--- init/index.js
+++ init/index.js
@@ -16,6 +16,7 @@ async function main() {
 
 const initDB = async () => {
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj)=> ({...obj, owner: "68aff9c680aa8b7dca318b17"}))
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
 }
--- routes/listing.js
+++ routes/listing.js
@@ -2,19 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Listing = require("../models/listing.js");
 const wrapAsync = require("../utils/wrapAsync.js");
-const ExpressError = require("../utils/ExpressError.js");
-const { listingSchema } = require("../schema.js");
-const { isLoggedIn } = require("../middleware.js");
-
-const validateListing = (req, res, next) => {
-  let { error } = listingSchema.validate(req.body);
-  if (error) {
-    let errMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(400, errMsg);
-  } else {
-    next();
-  }
-};
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 
 // Index Route
 router.get("/", async (req, res) => {
@@ -33,11 +21,19 @@ router.get(
   "/:id",
   wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
+    const listing = await Listing.findById(id)
+      .populate({
+        path: "reviews",
+        populate: {
+          path: "author",
+        },
+      })
+      .populate("owner");
     if (!listing) {
       req.flash("error", "Listing you requested for does not exist");
       return res.redirect("/listings");
     }
+    console.log(listing);
     res.render("listings/show.ejs", { listing });
   })
 );
@@ -45,7 +41,7 @@ router.get(
 // Create Route
 router.post(
   "/",
-  isLoggedIn, 
+  isLoggedIn,
   validateListing,
   wrapAsync(async (req, res, next) => {
     // let {title, description, image, price, country, location} = req.body;
@@ -56,6 +52,7 @@ router.post(
     // if(!newListing.title){
     //     throw new ExpressError(400, "Title is missing");
     // }
+    newListing.owner = req.user._id;
     await newListing.save();
     req.flash("success", "New Listing Created!");
     res.redirect("/listings");
@@ -66,6 +63,7 @@ router.post(
 router.get(
   "/:id/edit",
   isLoggedIn,
+  isOwner,
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
@@ -90,11 +88,11 @@ router.get(
 router.put(
   "/:id",
   isLoggedIn,
+  isOwner,
   validateListing,
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
-
     // This check: If image is currently a string, it replaces it with an object:
     if (typeof listing.image === "string") {
       listing.image = {
@@ -106,7 +104,6 @@ router.put(
     Object.assign(listing, req.body.listing);
     await listing.save();
     req.flash("success", "Listing Updated!");
-
     res.redirect(`/listings/${id}`);
   })
 );
@@ -115,6 +112,7 @@ router.put(
 router.delete(
   "/:id",
   isLoggedIn,
+  isOwner,
   wrapAsync(async (req, res) => {
     let { id } = req.params;
     let deleteListing = await Listing.findByIdAndDelete(id);
